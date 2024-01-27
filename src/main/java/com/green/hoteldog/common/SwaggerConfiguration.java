84372c41577a92e94d6e0fda1a188712d7ecbb43
Greenstudy <leejaewoong300@gmail.com>
2024-01-27 13:13:14 +0900

    재웅 01-24 0935 프로젝트 작업 업로드

--- src/main/java/com/green/hoteldog/common/SwaggerConfiguration.java
+++ src/main/java/com/green/hoteldog/common/SwaggerConfiguration.java
@@ -1,5 +1,10 @@
 package com.green.hoteldog.common;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -7,15 +12,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(title = "Dog Hotelling Service Ver.1"
+                ,description = "애견호텔링중개서비스 v1"
+                ,version = "1.0.0")
+        ,security = @SecurityRequirement(name = "authorization")
+)
+@SecurityScheme(
+        type = SecuritySchemeType.HTTP
+        ,name = "authorization"
+        ,in = SecuritySchemeIn.HEADER
+        ,bearerFormat = "JWT"
+        ,scheme = "Bearer"
+)
 public class SwaggerConfiguration {
-    @Bean
-    public OpenAPI openAPI(){
-        return new OpenAPI()
-                .components(new Components())
-                .info(new Info()
-                        .title("Dog Hotelling Service Ver.1")
-                        .description("애견호텔링중개서비스 v1")
-                        .version("2.0.0")
-                );
-    }
+
 }
