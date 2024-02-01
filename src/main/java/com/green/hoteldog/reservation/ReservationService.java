b3c04bb90fb8ab11fd920dd166844c8e458d24e8
홍길동 <hw05045@naver.com>
2024-02-01 10:48:07 +0900

    0201(영웅) - 오류 수정

--- src/main/java/com/green/hoteldog/reservation/ReservationService.java
+++ src/main/java/com/green/hoteldog/reservation/ReservationService.java
@@ -94,14 +94,11 @@ public class ReservationService {
 //                            })
 //                ).collect(Collectors.toList());
 
-        int affectedRows3 = mapper.updRemainedHotelRoom(updList);
         try {
-
+            int affectedRows3 = mapper.updRemainedHotelRoom(updList);
+            log.info("affectedRows3 : {}", affectedRows3);
         }catch (Exception e){
             throw new CustomException(ReservationErrorCode.NO_ROOMS_AVAILABLE_FOR_THIS_DATE);
-        }
-        if(affectedRows3 == 0){
-
         }
         return new ResVo(Const.SUCCESS);
     }
