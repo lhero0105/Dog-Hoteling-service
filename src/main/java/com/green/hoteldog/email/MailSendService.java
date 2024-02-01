0cc6636d33db7d964b3f8935e9ba15ce1b3ada73
Greenstudy <leejaewoong300@gmail.com>
2024-02-01 14:35:04 +0900

    재웅 02-01 1434 잔바리 수정

--- src/main/java/com/green/hoteldog/email/MailSendService.java
+++ src/main/java/com/green/hoteldog/email/MailSendService.java
@@ -72,7 +72,7 @@ public class MailSendService {
         String toMail = email;
         String title = "회원 가입 인증 이메일 입니다."; // 이메일 제목
         String content =
-                "나의 APP을 방문해주셔서 감사합니다." + 	//html 형식으로 작성 !
+                "저희 프로젝트 테스트에 참여해 주셔서 감사합니다." + 	//html 형식으로 작성 !
                         "<br><br>" +
                         "인증 번호는 " + authNumber + "입니다." +
                         "<br>" +
