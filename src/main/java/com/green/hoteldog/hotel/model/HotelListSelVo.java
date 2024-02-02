0864bc76fb017d5a203bb9dd5b0298ca1e71d2fd
홍길동 <hw05045@naver.com>
2024-02-02 16:49:49 +0900

    0202(영웅) - 오류 수정

--- src/main/java/com/green/hoteldog/hotel/model/HotelListSelVo.java
+++ src/main/java/com/green/hoteldog/hotel/model/HotelListSelVo.java
@@ -15,8 +15,8 @@ public class HotelListSelVo {
     private String addressName;
     @JsonProperty(value = "hotel_pic")
     private String hotelPic; // 가장 처음 등록한 사진
-    private float star; // 리뷰 평점 평균
-    private String price; // 하루 단위 최저가 방 가격
+    private float avgStar; // 리뷰 평점 평균
+    private String hotelRoomCost; // 하루 단위 최저가 방 가격
     @JsonProperty(value = "discount_per")
     private int discountPer;
     @JsonProperty(value = "book_mark")
